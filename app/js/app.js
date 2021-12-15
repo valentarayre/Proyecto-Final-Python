753af996ca366ae7d0e338b1a526c08a03bf08cf
valentarayre <valentintarayre@gmail.com>
2021-12-15 18:40:36 -0300

    vuejs

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+const Counter = {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+
+  }
+  
+  Vue.createApp(Counter).mount('#counter')
