f85fb89e7c82039d225f876a8ff128c94b4bacee
Marco Gregori <marco.gregori74@gmail.com>
2021-12-17 17:15:35 -0300

    Test
    Para testear todo, comente lo que falta de la ruta movie con metodo POST.

--- api/routes/Movies_routes.py
+++ api/routes/Movies_routes.py
@@ -6,4 +6,9 @@ def get_Movies():
     return jsonify({}), HTTPStatus.OK
 
 def post_Movies():
+#   if ("title" in movie) and ("sinopsis" in movie) and ("image_url" in movie) and ("id_director" in movie) and ("id_genero" in movie):
+#       if ( Checkear si hay ya existe esa pelicula )
+#           newMovie(movie["title"],movie["sinopsis"],movie["image_url"],movie["id_director"],movie["id_genero"])
+#           return jsonify({"message": "Pelicula Creada"}), HTTPStatus.OK
+#       return jsnonify({"message":"Pelicula ya existente"}), HTTPStatus.BAD_REQUEST
     return jsonify({"message":"Error Falta parametros"}), HTTPStatus.BAD_REQUEST
