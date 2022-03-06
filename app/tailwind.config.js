afd9ba242fc5aa0c0522386e0391cf3341324459
Marco Gregori <marco.gregori74@gmail.com>
2022-03-06 00:32:08 -0300

    fronttest

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    screens:{
+    'ssm': '475px',
+    // => @media (min-width: 420px) { ... }    
+    'sm': '576px',
+    // => @media (min-width: 576px) { ... }
+    'mdl': '576px',
+    // => @media (min-width: 576px) { ... }
+    'md': '960px',
+    // => @media (min-width: 960px) { ... }
+    'lg': '1440px',
+    // => @media (min-width: 1440px) { ... }}
+    },
+    extend: {
+      fontFamily:{
+        "Roboto" : ['Roboto', 'sans-serif'],
+        "Bebas" : ['Bebas Neue', 'cursive'],
+        "Mont" : ['Montserrat',' sans-serif']
+      },
+      colors: {
+        "MainColor" : "#080F28",
+        "SecondaryColor" : "#141A32",
+        "AccentColor" : "#007AFF",
+        "LigthBlue" : "#366B95",
+        "LigthBlueLg" : "#00B6FF",
+        "LigthBlueText" : "#8da0bc",
+        "BlancoLeche" :"#FFFFF5"
+      },
+    },
   },
   plugins: [],
 }
